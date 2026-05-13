8faef576c96672d9855a071d1c60e04e8e7f7ae5
Alvi2002 <hasibsheikh400@gmail.com>
2026-05-14 02:20:33 +0600

    Create Home component with BottomNav

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import BottomNav from '../components/BottomNav';
+export default function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-neonBlue">Live Sports</h1>
+      <BottomNav />
+    </div>
+  );
+}
