4b7dfe0ff10709a4c2d9fb31d45ba53a8cb2372b
Alvi2002 <hasibsheikh400@gmail.com>
2026-05-14 02:24:17 +0600

    Create AdminForm component for match input

---
+++ src/components/AdminForm.js
@@ -0,0 +1,8 @@
+export default function AdminForm() {
+  return (
+    <div className="glass p-4 rounded-xl">
+      <input className="w-full bg-white/10 p-2 rounded mb-2" placeholder="Match Title" />
+      <button className="w-full bg-neonBlue text-black p-2 rounded">Add Match</button>
+    </div>
+  );
+}
