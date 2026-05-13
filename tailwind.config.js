253eba1bbd2064dde9c05942e757c6b6752222d0
Alvi2002 <hasibsheikh400@gmail.com>
2026-05-14 02:59:18 +0600

    Update Tailwind CSS content paths for configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content:[
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -9,4 +12,4 @@ module.exports = {
     },
   },
   plugins:[],
-};
+}
