f6582f6d0c22d876d9c5502382035e23ac2ecfa7
Alvi2002 <hasibsheikh400@gmail.com>
2026-05-14 02:21:16 +0600

    Add BottomNav component for navigation

---
+++ src/components/BottomNav.js
@@ -0,0 +1,10 @@
+export default function BottomNav() {
+  return (
+    <div className="fixed bottom-0 left-0 w-full bg-black/90 backdrop-blur-lg border-t border-white/10 p-4 flex justify-between">
+      <span>Home</span>
+      <span>Live</span>
+      <span>Schedule</span>
+      <span>Admin</span>
+    </div>
+  );
+}
